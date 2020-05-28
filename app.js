61a529eb027f6974e1820ec080afdb59481b2159
Maria <andreeva.maria.s@gmail.com>
2020-05-28 22:45:43 +0300

    forth commit

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
-const express = require('express')
-const app = express()
-//var bodyParser = require('body-parser');
 
-
-app.use(express.static(__dirname + '/public'));
-
-app.get("/", function(req, res) {
-  console.log("Something was catched!" + req.method);
-  res.sendFile('public/index.html', {root: __dirname });
-});
-
-
-/*app.get("/", function(req, res) {
-    res.render("page11360025.html");
-});*/
-
-app.listen(8000, () => console.log('Server ready'))
 
 
 
